0135252c7174d1e5c9376ec5f8e0b92d56fd9e40
bibekgupta07 <phenomenalbibek@gmail.com>
2024-01-11 16:05:05 +0545

    currencyconvertor

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -4,13 +4,14 @@ import { useState } from "react"
 function App() {
   const [color, setColor] = useState("olive")
 
+
   return (
     <>
-     <div className="w-full h-screen duration-200"
+     <div className={`w-Full h-screen duration-200`}
      style={{backgroundColor: color}}
      > 
      <div className="fixed flex flex-wrap justify-center 
-     bottom-12 insert-x-0 px-2 ">
+     bottom-12 insert-x-0 px-2">
       <div className="flex flex-wrap justify-center gap-3 shadow-lg
        bg-white px-3 py-2 rounded-3xl"> 
 
